ec2a0826ca21e19fca1ed689a8c9773581ba30cb
naeem <naeemqg1@gmail.com>
2023-01-05 23:09:27 +0530

    yarn vite again

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { createBrowserRouter, RouterProvider} from "react-router-dom";
-import Root from "./routes/root";
-import { loader as rootLoader, action as rootAction } from "./routes/root";
-import ErrorPage from "./routes/error-page"
-import Contact from "./routes/contact";
-const router = createBrowserRouter([{
-  path:"/",
-  element: <Root/>,
-  errorElement: <ErrorPage/>,
-  loader:rootLoader,
-  action:rootAction,
-  children:[{
-    path:"/contacts/:contactId",
-    element:<Contact/>
-  }]
-}
-])
-ReactDOM.createRoot(document.querySelector("#root")).render(
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 )
